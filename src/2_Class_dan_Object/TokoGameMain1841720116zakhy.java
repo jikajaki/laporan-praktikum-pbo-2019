85157b70759e7912a1fa46ec9410672fdb3a1064
jikajaki <54810115+jikajaki@users.noreply.github.com>
2019-09-10 09:45:56 +0700

    Add files via upload

---
+++ src/2_Class_dan_Object/TokoGameMain1841720116zakhy.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ahmad Muzakhy
+ */
+public class TokoGameMain1841720116zakhy {
+      public static void main(String[] args){
+    
+        TokoGame1841720116zakhy tg1 = new TokoGame1841720116zakhy();
+        TokoGame1841720116zakhy tg2 = new TokoGame1841720116zakhy();
+        
+        tg1.id          = "1841720116";
+        tg1.nama        = "Ahmad Muzakhy";
+        tg1.game        = "PES 2019";
+        tg1.hargasewa   = 150000;
+        tg1.durasisewa  = 5;
+        tg1.peminjamanzakhy();
+        tg1.tampilanpeminjamanzakhy();
+        tg2.id          = "123456789";
+        tg2.nama        = "Pingki Widya Astuti";
+        tg2.game        = "FIFA 2019";
+        tg2.hargasewa   = 100000;
+        tg2.durasisewa  = 10;
+        tg2.peminjamanzakhy();
+        tg2.tampilanpeminjamanzakhy();
+    }
+}
